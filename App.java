909d9ec72baab7102aa156654a1c1a19745c71b8
Rexeth <106716657+Rexeth@users.noreply.github.com>
2022-10-25 19:55:44 +0800

    Add files via upload

--- App.java
+++ App.java
@@ -1,189 +1,48 @@
 public class App {
     public static void main(String[] args) throws Exception {
-  
-        char [][] mdArray = {
-            {'P','R','E','Y'},
-            {'L','A','V','A'},
-            {'O','V','E','R'},
-            {'T','E','N','D'}
-        };
-    
-        // WORD 1
-        System.out.println("1. WORD 1");
-        for(int row = 0; row < 4; row++)
+       { 
+        int loop = 0;
 
-        for(int column = 0; column < 4; column++){
+         while (loop != 3){
+            System.out.println("ACE");
+            loop ++;
+         }     
+        
+        // do while loop
 
-            if (row ==  0){
+         int j = 0;
+         do {
+            System.out.println("VIRAY");
+             j ++;
+        } while (j != 4);
 
-                System.out.print(mdArray[row][column]);
+        //for loop
 
-           } else
-
-                System.out.print("");
+        for (int t = 0; t != 5;  t++) {
+            System.out.println("SALIENDRA");
         }
+        // char array
+        
+        char[] myFirstName = {'A','C','E'};
 
-        // WORD 2
-        System.out.println("");
-        System.out.println("2. WORD 2");
-        for(int row = 0; row < 4; row++)
+         for (int num = 0; num < myFirstName.length; num++) {
 
-        for(int column = 0; column < 4; column++){
+            System.out.print(myFirstName[num]);
 
-            if (row ==  1){
-
-                System.out.print(mdArray[row][column]);
-
-           } else
-
-                System.out.print(""); 
-        }
-
-        // WORD 3
-        System.out.println("");
-        System.out.println("3. WORD 3");
-        for(int row = 0; row < 4; row++)
-
-        for(int column = 0; column < 4; column++){
-
-            if (row ==  2){
-
-                System.out.print(mdArray[row][column]);
-
-           } else
-
-                System.out.print("");
-        }
-
-        // WORD 4
-        System.out.println("");
-        System.out.println("4. WORD 4");
-        for(int row = 0; row < 4; row++)
-
-        for(int column = 0; column < 4; column++){
-
-            if (row ==  3){
-
-                System.out.print(mdArray[row][column]);
-
-           } else
-
-                System.out.print("");
-        }
-
-        // WORD 5
-        System.out.println("");
-        System.out.println("5. WORD 5");
-        for(int row = 0; row < 4; row++)
-
-        for(int column = 0; column < 4; column++){
-
-            if (column ==  0){
-
-                System.out.print(mdArray[row][column]);
-
-           } else
-
-                System.out.print("");
-        }
-
-        // WORD 6
-        System.out.println("");
-        System.out.println("6. WORD 6");
-        for(int row = 0; row < 4; row++)
-
-        for(int column = 0; column < 4; column++){
-
-            if (column ==  1){
-
-                System.out.print(mdArray[row][column]);
-
-           } else
-
-                System.out.print("");
-        }
-
-        // WORD 7
-        System.out.println("");
-        System.out.println("7. WORD 7");
-        for(int row = 0; row < 4; row++)
-
-        for(int column = 0; column < 4; column++){
-
-            if (column ==  2){
-
-                System.out.print(mdArray[row][column]);
-
-           } else
-
-                System.out.print("");
-        }
-
-        // WORD 8
-        System.out.println("");
-        System.out.println("8. WORD 8");
-        for(int row = 0; row < 4; row++)
-
-            for(int column = 0; column < 4; column++){
-
-                if (column ==  3){
-
-                    System.out.print(mdArray[row][column]);
-
-               } else
-
-                    System.out.print("");
-            }
-
-        // WORD 9
-        System.out.println("");
-        System.out.println("9. WORD 9");
-
-        for (int rows = 0; rows < 4; rows++) {
-            System.out.println(" ");
-            for (int columns = 0; columns < 4; columns++) {
-
-                if (rows == 1 && (columns == 1 || columns == 2)) {
-                    System.out.print(" ");
-                }
-                else if (rows == 2 && (columns == 1 || columns == 2)) {
-                    System.out.print(" ");
-                }
-                else {
-                    System.out.print(mdArray[rows][columns]);
-                }
-            }
-        }
-
-         // WORD 10
-         System.out.println("");
-         System.out.println("10. WORD 10");
-
-         for (int rows = 0; rows < 4; rows++) {
-             System.out.println(" ");
-             for (int columns = 0; columns < 4; columns++) {
-
-         if (rows == 0 && (columns == 1 || columns == 2)) {
-System.out.print(" ");
-}
-
-         else if (rows == 1 && (columns == 0 || columns == 3)) {
-             System.out.print(" "); 
          }
 
-         else if (rows == 2 && (columns == 0 || columns == 3)) {
-             System.out.print(" ");
-        }
+        
 
-         else if (rows == 3 && (columns == 1 || columns == 2)) {
-             System.out.print(" "); 
-         }
+        System.out.println();
 
-         else{
-             System.out.print(mdArray[rows][columns]);
-        }
+         // firstname reverse
 
-        }
+         for (int num = myFirstName.length-1; num >= 0; num--) 
+
+            System.out.print(myFirstName[num]);
+
+        
+
     }
 
 }
